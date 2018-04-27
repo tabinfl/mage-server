tabinfl/mage-server

commit c0be955d49807a02d37eea18e0f0dc6fb522cb51

fixed an undefined reference error because require.main.filename did not resolve correctly when running the migration script through npm

api/icon.js
@@ -7,7 +7,7 @@ var IconModel = require('../models/icon')
   , archiver = require('archiver')
   , environment = require('../environment/env');
 
-var appRoot = path.dirname(require.main.filename);
+var appRoot = path.resolve(__dirname, '..');
 var iconBase = environment.iconBaseDirectory;
 
 function Icon(eventId, formId, primary, variant) {

commit 9a6e30102fb988015ef0e5e61bb012c01228f842

fixed a failing test in eventCreateTest.js caused by change from commit c0be955d49807a02d37eea18e0f0dc6fb522cb51 which rendered the mocked file system path of the default icon erroneous and caused an error when the api/events POST operation attempted to call api.Icon.setDefaultIcon()

api/icon.js
@@ -1,4 +1,4 @@
-var IconModel = require('../models/icon')
+const IconModel = require('../models/icon')
   , mongoose = require('mongoose')
   , log = require('winston')
   , path = require('path')
@@ -7,8 +7,9 @@ var IconModel = require('../models/icon')
   , archiver = require('archiver')
   , environment = require('../environment/env');
 
-var appRoot = path.resolve(__dirname, '..');
-var iconBase = environment.iconBaseDirectory;
+const appRoot = path.resolve(__dirname, '..');
+const iconBase = environment.iconBaseDirectory;
+const defaultIconPath = path.join(appRoot, 'public/img/default-icon.png');
 
 function Icon(eventId, formId, primary, variant) {
   this._eventId = eventId || null;
@@ -90,31 +91,28 @@ Icon.prototype.getIcon = function(callback) {
   };
 
   IconModel.getIcon(options, function(err, icon) {
-    if (err || !icon) return callback(err);
-
+    if (err || !icon) {
+      return callback(err);
+    } 
     icon.path = path.join(iconBase, icon.relativePath);
     callback(err, icon);
   });
 };
 
-Icon.prototype.setDefaultIcon = function(callback) {
-  var relativePath = createIconPath(this, 'default-icon.png');
-  var newIcon = {
+Icon.prototype.saveDefaultIconToEventForm = function(callback) {
+  const relativePath = createIconPath(this, 'default-icon.png');
+  const targetPath = path.join(iconBase, relativePath);
+  const newIcon = {
     eventId: this._eventId,
     formId: this._formId,
     relativePath: relativePath
   };
-
-  var iconPath = path.join(iconBase, relativePath);
-  fs.copy(path.join(appRoot, '/public/img/default-icon.png'), iconPath, function(err) {
-    if (err) return callback(err);
-
+  fs.copy(defaultIconPath, targetPath, function(err) {
+    if (err) {
+      return callback(err);
+    } 
     IconModel.create(newIcon, callback);
   });
-};
-
-Icon.prototype.getDefaultIcon = function(callback) {
-  return callback(null, path.join(appRoot, '/public/img/default-icon.png'));
 };
 
 Icon.prototype.create = function(icon, callback) {
@@ -201,5 +199,9 @@ Icon.prototype.delete = function(callback) {
     });
   });
 };
+
+Object.defineProperty(Icon, 'defaultIconPath', {
+  value: defaultIconPath
+});
 
 module.exports = Icon;

routes/events.js
@@ -216,14 +216,13 @@ module.exports = function(app, security) {
     parseEventQueryParams,
     function(req, res, next) {
       Event.create(req.body, req.user, function(err, event) {
-        if (err) return next(err);
-
-        //copy default icon into new event directory
-        new api.Icon(event._id).setDefaultIcon(function(err) {
+        if (err) {
+          return next(err);
+        }
+        new api.Icon(event._id).saveDefaultIconToEventForm(function(err) {
           if (err) {
             return next(err);
           }
-
           res.status(201).json(event);
         });
       });
@@ -343,8 +342,7 @@ module.exports = function(app, security) {
 
         async.parallel([
           function(done) {
-            //copy default icon into new event directory
-            new api.Icon(req.event._id, form._id).setDefaultIcon(function(err) {
+            new api.Icon(req.event._id, form._id).saveDefaultIconToEventForm(function(err) {
               done(err);
             });
           },
@@ -529,13 +527,7 @@ module.exports = function(app, security) {
     passport.authenticate('bearer'),
     authorizeAccess('READ_EVENT_ALL', 'read'),
     function(req, res, next) {
-      new api.Icon().getDefaultIcon(function(err, iconPath) {
-        if (err) return next(err);
-
-        if (!iconPath) return res.status(404).send();
-
-        res.sendFile(iconPath);
-      });
+      res.sendFile(api.Icon.defaultIconPath);
     }
   );
 

test/event/eventCreateTest.js
@@ -18,12 +18,14 @@ require('../../models/team');
 var TeamModel = mongoose.model('Team');
 
 require('../../models/event');
-var EventModel = mongoose.model('Event');
+const EventModel = mongoose.model('Event');
 
 require('../../models/icon');
-var IconModel = mongoose.model('Icon');
+const IconModel = mongoose.model('Icon');
 
 describe("event create tests", function() {
+
+  this.timeout(300000);
 
   var sandbox;
   before(function() {
@@ -34,7 +36,7 @@ describe("event create tests", function() {
     sandbox.restore();
   });
 
-  var userId = mongoose.Types.ObjectId();
+  const userId = mongoose.Types.ObjectId();
 
   it("should create event", function(done) {
     sandbox.mock(TokenModel)
@@ -44,17 +46,17 @@ describe("event create tests", function() {
       .chain('exec').atLeast(1)
       .yields(null, MockToken(userId, ['CREATE_EVENT']));
 
-    var eventId = 1;
+    const eventId = 1;
     sandbox.mock(CounterModel)
       .expects('findOneAndUpdate')
       .yields(null, { sequence: eventId });
 
-    var mockEvent = new EventModel({
+    const mockEvent = new EventModel({
       _id: eventId,
       name: 'Mock Event'
     });
 
-    var eventAcl = {};
+    const eventAcl = {};
     eventAcl[userId.toString()] = 'OWNER';
     sandbox.mock(EventModel)
       .expects('create')
@@ -67,14 +69,14 @@ describe("event create tests", function() {
       .expects('createCollection')
       .yields(null);
 
-    var teamId = mongoose.Types.ObjectId();
-    var mockTeam = {
+    const teamId = mongoose.Types.ObjectId();
+    const mockTeam = {
       _id: teamId,
       name: 'Mock Team',
       teamEventId: eventId
     };
 
-    var teamAcl = {};
+    const teamAcl = {};
     teamAcl[userId.toString()] = 'OWNER';
     sandbox.mock(TeamModel)
       .expects('create')
@@ -93,8 +95,8 @@ describe("event create tests", function() {
       .expects('findByIdAndUpdate').withArgs(eventId)
       .yields(null, mockEvent);
 
-    var defaultIcon = path.join(path.dirname(require.main.filename), '/public/img/default-icon.png');
-    var fs = {
+    const defaultIcon = require('../../api/icon').defaultIconPath;
+    const fs = {
       '/var/lib/mage': {}
     };
     fs[defaultIcon] = new Buffer([8, 6, 7, 5, 3, 0, 9]);
@@ -138,12 +140,12 @@ describe("event create tests", function() {
       .chain('exec').atLeast(1)
       .yields(null, MockToken(userId, ['CREATE_EVENT']));
 
-    var eventId = 1;
+    const eventId = 1;
     sandbox.mock(CounterModel)
       .expects('findOneAndUpdate')
       .yields(null, { sequence: eventId });
 
-    var mockEvent = new EventModel({
+    const mockEvent = new EventModel({
       _id: eventId
     });
     sandbox.mock(EventModel.collection)
@@ -170,12 +172,12 @@ describe("event create tests", function() {
       })
       .expect(400)
       .expect(function(res) {
-        var error = res.text;
+        let error = res.text;
         should.exist(error);
         error.should.be.a('string');
         error = JSON.parse(error);
         error.should.have.property('message').that.contains("Event validation failed");
-        var errors = error.errors;
+        const errors = error.errors;
         should.exist(errors.name);
         errors.name.should.have.property('path').that.equals('name');
         errors.name.should.have.property('message').that.equals('Path `name` is required.');